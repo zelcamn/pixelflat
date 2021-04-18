cd1905fff80a82cea62d1c9db049dc6406a89a01
zelcamn <zelcamn@yandex.ru>
2021-04-18 22:30:45 +0900

    wefwef

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-# Импортируем необходимые классы.
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
+
 
 def echo(update, context):
     try:
@@ -13,21 +13,14 @@ def echo(update, context):
 
 
 def main():
-    # Создаём объект updater.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
     updater = Updater('1515524053:AAFWWpQGYRb6rZ6UeVFhdFfviOHXTqzgkkY', use_context=True)
 
-    # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("draw", echo))
-    # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
 
-    # Ждём завершения приложения.
-    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
     updater.idle()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
